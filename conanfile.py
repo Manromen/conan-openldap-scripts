7c7994b6957cde0394f6b0235cfda2bced0a4910
Ralph-Gordon Paul <g.paul@appcom-interactive.de>
2019-06-12 16:05:28 +0200

    WIP travis config for ios

--- conanfile.py
+++ conanfile.py
@@ -22,80 +22,123 @@ class OpenLDAPConan(ConanFile):
     # compile using cmake
     def build(self):
         library_folder = "%s/openldap-%s" % (self.source_folder, self.version)
-        autotools = AutoToolsBuildEnvironment(self)
-        env_build_vars = autotools.vars
-        build_host = None
-
-        autotools_args = []
+        # autotools = AutoToolsBuildEnvironment(self)
+        # env_build_vars = autotools.vars
+        # build_host = None
 
         if self.settings.os == "iOS":
             if self.version == "2.4.46":
                 self.run("cd %s; patch -p1 < %s/patches-ios/2.4.46.patch" % (library_folder, self.build_folder))
 
-            build_host = "i386-apple-darwin"
+            # define all architectures for ios fat library
+            if "arm" in self.settings.arch:
+                self.variants = ["armv7", "armv7s", "armv8"]
+                for arch in self.variants:
+                    self.build_ios(library_folder=library_folder, arch=arch)
+                self.create_ios_fat_files()
+            else:
+                self.variants = []
 
-            platform_path = "/Applications/Xcode.app/Contents/Developer/Platforms"
-            platform = "iPhoneOS"
-            if self.settings.arch == "x86" or self.settings.arch == "x86_64":
-                platform = "iPhoneSimulator"
 
-            # CC
-            mybuf = StringIO()
-            self.run("xcrun -sdk iphoneos -find clang", output=mybuf)
-            env_cc = mybuf.getvalue()
-            env_build_vars['CC'] = env_cc
-            mybuf.close()
-            print("CC", env_cc)
+    def build_ios(self, library_folder=None, arch=None):
 
-            # CPP
-            env_cpp = "%s -E" % env_cc
-            env_build_vars['CPP'] = env_cpp
-            print("CPP", env_cpp)
+        if arch == None:
+            raise Exception("build_ios called without a given arch")
 
-            # CFLAGS
-            env_cflags = "-arch armv7 -arch armv7s -arch arm64 -isysroot %s/%s.platform/Developer/SDKs/%s%s.sdk -miphoneos-version-min=10" % (platform_path, platform, platform, self.settings.os.version)
-            env_build_vars['CFLAGS'] = env_cflags
-            print("CFLAGS", env_cflags)
+        autotools = AutoToolsBuildEnvironment(self)
+        env_build_vars = autotools.vars
+        autotools_args = ["--with-yielding_select=no", "--with-tls=auto", "--disable-slapd"]
 
-            # AR
-            mybuf = StringIO()
-            self.run("xcrun -sdk iphoneos -find ar", output=mybuf)
-            env_ar = mybuf.getvalue()
-            env_build_vars['AR'] = env_ar
-            mybuf.close()
-            print("AR", env_ar)
-            
-            # RANLIB
-            mybuf = StringIO()
-            self.run("xcrun -sdk iphoneos -find ranlib", output=mybuf)
-            env_ranlib = mybuf.getvalue()
-            env_build_vars['RANLIB'] = env_ranlib
-            mybuf.close()
-            print("RANLIB", env_ranlib)
+        build_host = "%s-apple-darwin" % tools.to_apple_arch(arch)
+        if arch == "armv8":
+            build_host = "arm-apple-darwin"
 
-            #self.output.warn(mybuf.getvalue())
+        platform_path = "/Applications/Xcode.app/Contents/Developer/Platforms"
+        platform = "iPhoneOS"
+        if self.settings.arch == "x86" or self.settings.arch == "x86_64":
+            platform = "iPhoneSimulator"
 
-            #export CPPFLAGS="-arch ${target} -isysroot $PLATFORMPATH/$platform.platform/Developer/SDKs/$platform$IOS_SDK_VERSION.sdk -miphoneos-version-min=$IOS_MIN_VERSION"
-            #export LDFLAGS="-arch ${target} -isysroot $PLATFORMPATH/$platform.platform/Developer/SDKs/$platform$IOS_SDK_VERSION.sdk"
+        # CC
+        cmd_output = StringIO()
+        self.run("xcrun -sdk iphoneos -find clang", output=cmd_output)
+        env_cc = cmd_output.getvalue().rstrip()
+        env_build_vars['CC'] = env_cc
+        cmd_output.close()
+        print("CC", env_cc)
+        #autotools_args.append("CC=%s" % env_cc)
 
-            #self.run("export IOS_SDK_VERSION=$(xcodebuild -showsdks | grep iphoneos | awk '{print $4}' | sed 's/[^0-9,\.]*//g'")
+        # CPP
+        env_cpp = "%s -E" % env_cc
+        env_build_vars['CPP'] = env_cpp
+        print("CPP", env_cpp)
+        #autotools_args.append("CPP=%s" % env_cpp)
 
-            #self.run("export LIPO_CMD=$(xcrun -sdk iphoneos -find lipo)")
-            #self.run("export CC='$(xcrun -sdk iphoneos -find clang)'")
-            #self.run("export CPP='$CC -E'")
-            #self.run('export CFLAGS="-arch ${target} -I${ABSOLUTE_DIR}/ios/include -isysroot $PLATFORMPATH/$platform.platform/Developer/SDKs/$platform$IOS_SDK_VERSION.sdk -miphoneos-version-min=$IOS_MIN_VERSION"')
-            #self.run("export AR=$(xcrun -sdk iphoneos -find ar)")
-            #self.run("export RANLIB=$(xcrun -sdk iphoneos -find ranlib)")
+        # CFLAGS
+        env_cflags = "-arch %s -isysroot %s/%s.platform/Developer/SDKs/%s%s.sdk -miphoneos-version-min=10" % (tools.to_apple_arch(arch), platform_path, platform, platform, self.settings.os.version)
+        env_build_vars['CFLAGS'] = env_cflags
+        print("CFLAGS", env_cflags)
+        #autotools_args.append("CFLAGS=%s" % env_cflags)
 
-            autotools_args = ["--disable-shared", "--with-yielding_select=no", "--with-tls=auto", "--disable-slapd"]
+        # CPPFLAGS
+        env_cppflags = "-arch %s -isysroot %s/%s.platform/Developer/SDKs/%s%s.sdk -miphoneos-version-min=10" % (tools.to_apple_arch(arch), platform_path, platform, platform, self.settings.os.version)
+        env_build_vars['CPPFLAGS'] = env_cppflags
+        print("CPPFLAGS", env_cppflags)
+        #autotools_args.append("CPPFLAGS=%s" % env_cppflags)
 
+        # LDFLAGS
+        env_ldflags = "-arch %s -isysroot %s/%s.platform/Developer/SDKs/%s%s.sdk" % (tools.to_apple_arch(arch), platform_path, platform, platform, self.settings.os.version)
+        env_build_vars['LDFLAGS'] = env_ldflags
+        print("LDFLAGS", env_ldflags)
+        #autotools_args.append("LDFLAGS=%s" % env_ldflags)
+
+        # AR
+        cmd_output = StringIO()
+        self.run("xcrun -sdk iphoneos -find ar", output=cmd_output)
+        env_ar = cmd_output.getvalue().rstrip()
+        env_build_vars['AR'] = env_ar
+        cmd_output.close()
+        print("AR", env_ar)
+        #autotools_args.append("AR=%s" % env_ar)
+        
+        # RANLIB
+        cmd_output = StringIO()
+        self.run("xcrun -sdk iphoneos -find ranlib", output=cmd_output)
+        env_ranlib = cmd_output.getvalue().rstrip()
+        env_build_vars['RANLIB'] = env_ranlib
+        cmd_output.close()
+        print("RANLIB", env_ranlib)
+        #autotools_args.append("RANLIB=%s" % env_ranlib)
+        
+        prefix_folder = os.path.join(self.build_folder, "output")
+        exec_prefix_folder = os.path.join(self.build_folder, "output", arch)
+        autotools_args.append("--prefix=%s" % prefix_folder)
+        autotools_args.append("--exec-prefix=%s" % exec_prefix_folder)
+        #autotools_args.append("--target=%s-apple-darwin" % tools.to_apple_arch(arch))
+        
         if self.settings.build_type == "Release":
             autotools_args.append("--disable-debug")
         if self.options.shared == False:
             autotools_args.append("--disable-shared")
+            autotools_args.append("--enable-static")
       
-        autotools.configure(configure_dir=library_folder, args=autotools_args, host=build_host, vars=env_build_vars, build=False)
+        autotools.configure(configure_dir=library_folder, args=autotools_args, host=build_host, vars=env_build_vars, build=None, target=None)
         autotools.make()
+
+    def create_ios_fat_files(self):
+        # LIPO command
+        cmd_output = StringIO()
+        self.run("xcrun -sdk iphoneos -find lipo", output=cmd_output)
+        lipo_cmd = cmd_output.getvalue().rstrip()
+        cmd_output.close()
+
+        lib_dir = os.path.join(self.build_folder,"output","armv8","lib")
+        for f in os.listdir(lib_dir):
+            lipo = lipo_cmd + " -create "
+            if f.endswith(".a"):
+                for arch in self.variants:
+                    lipo += os.path.join(self.build_folder,"output",arch,"lib", f, " ")
+                lipo += "-output " + os.path.join(self.package_folder, "lib", f)
+                self.run(lipo)
 
     def package(self):
         self.copy("*", dst="include", src='include')
